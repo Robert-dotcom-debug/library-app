bfbb256ff13d019a3a2200bd40e7513e85c7bed4
Robert-dotcom-debug <robertoroly59@gmail.com>
2026-05-02 21:44:28 -0400

    feat: rutas resource, controladores y vistas Blade con componentes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
-Route::get('/', function () {
+
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::redirect('/', '/books');
+Route::redirect('/Books', '/books');
+Route::redirect('/Authors', '/authors');
+
+Route::resource('books', BookController::class);
+
+Route::resource('authors', AuthorController::class);
